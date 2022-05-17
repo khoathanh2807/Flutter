44ec147b417fcc92551bdc7277b5a6092f8954e2
khoathanh2807 <khoathanh2807@gmail.com>
2022-05-18 04:16:59 +0700

    reset repository

--- flutter_bottom_nav_bar_app/android/app/src/main/kotlin/com/khoathanh2807/flutter_bottom_nav_bar_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.khoathanh2807.flutter_bottom_nav_bar_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_sakai_service_app/android/app/src/main/kotlin/com/khoathanh2807/flutter_sakai_service_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.khoathanh2807.flutter_sakai_service_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
